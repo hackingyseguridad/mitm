02a2927a163ea5ab4e466b27b8cbfbe656d49b7b
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-10-04 23:35:11 +0200

    Update netspy.sh

--- netspy.sh
+++ netspy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo 
-netdiscover -i $INTERNFACE
-bettercap -iface $INTERFACE -eval "net.probe on; ticker on "
-bettercap -iface $INTERFACE -eval "net.probe on; set arp.spoof.targets; arp.spoof on; set net.sniff.verbose false; net.sniff on"
+bettercap -X
